349ebd9d170b1a5d55cc11df77f4bebe6e1d8384
Jaekyeom Kim <btapiz@gmail.com>
2022-01-11 21:30:09 +0900

    Implement listing and focusing open Typora documents.

---
+++ src/focus-doc.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env osascript -l JavaScript
+
+function run(args) {
+  const app = Application("Typora");
+  const wasRunning = app.running();
+  app.activate();
+  if (wasRunning) {
+    let name = undefined;
+    const windowId = parseInt(args[0]);
+    for (let w = 0; w < app.windows.length; w++) {
+      const win = app.windows[w];
+      if (win.id() == windowId) {
+        name = win.document().name();
+        win.index = 1;
+        break;
+      }
+    }
+    if (name !== undefined) {
+      // TODO: A reliable way to activate the corresponding window.
+      const sys = Application("System Events");
+      const windows = sys.processes["Typora"].windows;
+      for (let w = 0; w < windows.length; w++) {
+        if (windows[w].name() == name) {
+          windows[w].actions['AXRaise'].perform();
+          break;
+        }
+      }
+    }
+  }
+}
+
---
+++ src/list-docs.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env osascript -l JavaScript
+
+function run() {
+  const app = Application("Typora");
+  if (!app.running()) {
+    return JSON.stringify({
+      items: [
+        {
+          title: `Typora is not running`,
+          subtitle: `Press enter to launch Typora`,
+        },
+      ],
+    });
+  }
+
+  const docsMap = {};
+  for (let w = 0; w < app.windows.length; w++) {
+    const win = app.windows[w];
+    const doc = win.document();
+
+    if (doc !== undefined) {
+      const name = doc.name();
+      const path = doc.path();
+
+      docsMap[path] = {
+        uid: path,
+        title: name,
+        subtitle: path,
+        quicklookurl: path,
+        match: `${name} ${path.replace(
+          /[^A-Za-z0-9]/g,
+          " ",
+        )}`,
+        arg: `${win.id()}`,
+      };
+    }
+  }
+
+  const items = Object.keys(docsMap).reduce((acc, path) => {
+    acc.push(docsMap[path]);
+    return acc;
+  }, []);
+
+  return JSON.stringify({ items });
+}
+
