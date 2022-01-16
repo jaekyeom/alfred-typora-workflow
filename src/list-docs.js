c939a7a211499466e84555e9c47c717156a89a7d
Jaekyeom Kim <btapiz@gmail.com>
2022-01-16 19:44:52 +0900

    Support querying markdown files from the same Git repositories or directories (when `LIST_ONLY_OPEN_DOCS` is 0).

--- src/list-docs.js
+++ src/list-docs.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env osascript -l JavaScript
 
 function run() {
+  ObjC.import('stdlib');
+  ObjC.import('Foundation');
+
   const app = Application("Typora");
   if (!app.running()) {
     return JSON.stringify({
@@ -8,12 +11,20 @@ function run() {
         {
           title: `Typora is not running`,
           subtitle: `Press enter to launch Typora`,
+          variables: {
+            isOpen: true,
+          },
         },
       ],
     });
   }
 
-  const docsMap = {};
+  const listOnlyOpenDocs = parseInt($.getenv('LIST_ONLY_OPEN_DOCS'));
+
+  const items = [];
+  const addedDocs = new Set();
+  const dirs = [];
+  const addedDirs = new Set();
   for (let w = 0; w < app.windows.length; w++) {
     const win = app.windows[w];
     const doc = win.document();
@@ -24,31 +35,74 @@ function run() {
       const path = doc.path() || name;
       const hasPath = !!doc.path();
 
-      docsMap[path] = {
-        uid: path,
-        title: name,
-        subtitle: path,
-        quicklookurl: (hasPath ? path : null),
-        match: `${name} ${path.replace(
-          /[^A-Za-z0-9]/g,
-          " ",
-        )}`,
-        arg: `${win.id()}`,
-        mods: {
-          alt: {
-            valid: true,
-            arg: `${win.id()}`,
-            subtitle: `Close ${name}${doc.modified() ? " (Edited)" : ""}`,
+      if (!addedDocs.has(path)) {
+        items.push({
+          title: name,
+          subtitle: path,
+          quicklookurl: (hasPath ? path : null),
+          match: `${name} ${path.replace(
+            /[^A-Za-z0-9]/g,
+            " ",
+          )}`,
+          icon: { path: 'icon.png' },
+          arg: `${win.id()}`,
+          mods: {
+            alt: {
+              valid: true,
+              arg: `${win.id()}`,
+              subtitle: `Close ${name}${doc.modified() ? " (Edited)" : ""}`,
+            }
+          },
+          variables: {
+            isOpen: true,
+          },
+        });
+        addedDocs.add(path);
+
+        if (!listOnlyOpenDocs && hasPath) {
+          const dir = $.NSString.alloc.initWithUTF8String(path).stringByDeletingLastPathComponent.js + '/';
+          if (!addedDirs.has(dir)) {
+            dirs.push(dir);
+            addedDirs.add(dir);
           }
         }
-      };
+      }
     }
   }
 
-  const items = Object.keys(docsMap).reduce((acc, path) => {
-    acc.push(docsMap[path]);
-    return acc;
-  }, []);
+  const processedDirs = new Set();
+  if (!listOnlyOpenDocs) {
+    var curr = Application.currentApplication();
+    curr.includeStandardAdditions = true;
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = curr.doShellScript(`cd '${dirs[i].replace(/'/g, "'\\''")}'; git rev-parse --show-toplevel || pwd`);
+      if (!processedDirs.has(dir)) {
+        const mdFiles = curr.doShellScript(`cd '${dirs[i].replace(/'/g, "'\\''")}'; gd="$(git rev-parse --show-toplevel)" && git ls-files --full-name --cached --others "$gd"/**/*.md || ls *.md`).split('\r');
+        for (let d = 0; d < mdFiles.length; d++) {
+          const name = mdFiles[d].split('/').pop();
+          const path = dir + '/' + mdFiles[d];
+          if (!addedDocs.has(path)) {
+            items.push({
+              title: name,
+              subtitle: path,
+              quicklookurl: path,
+              match: `${name} ${path.replace(
+                /[^A-Za-z0-9]/g,
+                " ",
+              )}`,
+              icon: { path: 'icon_dimmed.png' },
+              arg: path,
+              variables: {
+                isOpen: false,
+              },
+            });
+            addedDocs.add(path);
+          }
+        }
+        processedDirs.add(dir);
+      }
+    }
+  }
 
   return JSON.stringify({ items });
 }
