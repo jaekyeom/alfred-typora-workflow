05b785facc2a1daacd0c76e268764c5a51c69a52
Jaekyeom Kim <btapiz@gmail.com>
2025-12-31 02:22:22 -0500

    Update markdown link generation.

--- src/get-markdown-links-to-files.js
+++ src/get-markdown-links-to-files.js
@@ -50,12 +50,13 @@ const getRelativePath = function(path, anchorPath) {
 const getMarkdownLink = function(path, anchorPath) {
   const escapedText = (path.length == 0
     ? '/'
-    : path[path.length - 1].replace(/([#_*/()<>])/g, '\\$1').replace(/([\[\]])/g, '\\\\$1'));
+    : path[path.length - 1].replace(/([#_*/<>])/g, '\\$1').replace(/([\[\]])/g, '\\\\$1'));
   const escapedPath = (getRelativePath(path, anchorPath)
     .replace(/[(]/g, '%28')
     .replace(/[)]/g, '%29')
     .replace(/\[/g, '%5B')
-    .replace(/\]/g, '%5D'));
+    .replace(/\]/g, '%5D')
+    .replace(/ /g, '%20'));
   return `[${escapedText}](${escapedPath})`;
 };
 
